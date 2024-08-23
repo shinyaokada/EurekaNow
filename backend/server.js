d553d1224be617b423787ec2843a4c5f9370d872
shinyaokada <okadashinya4523@gmail.com>
2024-08-23 17:15:10 +0900

    update server.js received message

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,11 @@ app.use(cors({
 }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Received request:', req.method, req.url);
+  next();
+});
+
 // ユーザーの取得
 app.get('/users', async (req, res) => {
   const users = await User.find();
